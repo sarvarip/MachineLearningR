154a92f1842d491eb15ea524e52b283dcb9c6591
sarvarip <sarvarip@gmail.com>
2019-07-10 23:49:30 -0700

    Fixed repeated double cross validation

--- classification/classification_example.r
+++ classification/classification_example.r
@@ -10,7 +10,6 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-
 library(glmnet)
 library(magrittr)
 library(glmnet)
@@ -18,6 +17,27 @@ library(ROCR)
 library(data.table)
 library(Hmisc)
 library(DMwR)
+
+get_performance_vals <- function(y_real, y_pred){
+    areaundercurve <- prediction(y_pred, y_real) %>% performance(measure = "auc") %>% .@y.values %>% as.numeric()
+    t <- table(y_real, as.integer((y_pred > 0.5))==y_real)
+    tn = t["0", "TRUE"] 
+    fn = t["1", "FALSE"]
+    tp = t["1", "TRUE"]
+    fp = t["0", "FALSE"]
+
+    dividecatch <- function(x,y) ifelse(y==0,0,base:::"/"(x,y))
+
+    precision = as.numeric(dividecatch(tp,(tp+fp)))
+    recall = as.numeric(dividecatch(tp,(tp+fn)))
+    fscore = as.numeric(dividecatch(2*precision*recall,(precision+recall)))
+    mcc = as.numeric(dividecatch((tp*tn-fp*fn),sqrt((tp+fp)*(tp+fn)*(tn+fp)*(tn+fn))))
+    specificity = as.numeric(dividecatch(tn,(tn+fp)))
+    accuracy = as.numeric(dividecatch((tp+tn),(tp+tn+fp+fn)))
+
+    my_list <- list("auc" = areaundercurve, "precision" = precision, "recall" = recall, "fscore" = fscore, "mcc" = mcc, "specificity" = specificity, "accuracy" = accuracy)
+    return (my_list)
+}
 
 df <- read.delim("13_methylation", header = TRUE, sep = "\t")
 rownames(df) <- df$X
@@ -28,11 +48,16 @@ df_s <- df_s[!(is.na(df_s$Ratio) | df_s$Ratio==""), ]
 rownames(df_s) <- df_s$Laboratory.ID
 df_s$Laboratory.ID <- NULL
 dat <- merge(df, df_s, by=0)
+dat$Numsuccess <- NULL
+dat$Numtrials <- NULL
 
-f1_list <- rep(0,20)
-recall_list <- rep(0,20)
-specificity_list <- rep(0,20)
-auc_list <- rep(0,20)
+f1_list <- list()
+recall_list <- list()
+specificity_list <- list()
+auc_list <- list()
+mcc_list <- list()
+accuracy_list <- list()
+precision_list <- list()
 
 opt.cut = function(perf, pred){
     cut.ind = mapply(FUN=function(x, y, p){
@@ -43,15 +68,16 @@ opt.cut = function(perf, pred){
     }, perf@x.values, perf@y.values, pred@cutoffs)
 }
 
+for (j in 1:10) {
 
-for (j in 1:20) {
+    print("Outer iteration number: ")
+    print(j)
 
     data <- dat[sample(nrow(dat)),]
     data$Ind = rownames(data)
     rownames(data) <- data$Row.names
     data$Row.names <- NULL
     data$euploid <- (data$Ratio==0) %>% as.integer() %>% as.factor()
-
 
     cv_fold = 5
     cv_Ind = list()
@@ -65,78 +91,103 @@ for (j in 1:20) {
     cv_Ind %<>% rbindlist() %>% setorder(Ind)
     data %>% setorder(Ind)
 
-    #making sure that the number of original and synthetic samples in each fold is roughly the same
-
     data$Ratio <- NULL
-    data$Numsuccess <- NULL
-    data$Numtrials <- NULL
     data$Ind <- NULL
-
-    for (i in 1:4) {
-        foldindx = cv_Ind$fold == i
-        myfold <- data[foldindx,]
-        mysmote <- SMOTE(euploid ~ ., data = myfold)
-        numsamples = length(mysmote$euploid)
-        if (i==1){
-            foldid <- rep(1,numsamples)
-            smote_train = mysmote
-        } else{
-            foldid <- append(foldid, rep(i,numsamples))
-            smote_train <- rbind(smote_train, mysmote)
-        }
-    }
-
     
-    #cv_Ind_train = cv_Ind$fold != 5
-    #data_train = data[cv_Ind_train,]
-    #smote_train <- SMOTE(euploid ~ ., data  = data_train)
-    print(table(smote_train$euploid))
-
+    cv_Ind_train = cv_Ind$fold != 5
+    data_train = data[cv_Ind_train,]
     cv_Ind_test = cv_Ind$fold ==5
     data_test = data[cv_Ind_test,]
 
-    Y_ <- smote_train$euploid
-    X_ <- smote_train
-    X_$euploid <- NULL
-    X <- data.matrix(X_)
-    Y <- Y_
+    score_params = list()
+    model_params = list()
 
+    lambdas_to_try <- 10^seq(-6, -1, length.out = 10)
+    for (lambda in seq(lambdas_to_try)) {
 
-    cv0 = cv.glmnet(X,Y,family = "binomial", type.measure="deviance", foldid=foldid, standardize=T, alpha=0)
+        score_fold = list()
+        
+        cv_fold = 5
+        cv_Ind = list()
+        data_train$Ind = seq(length=nrow(data_train))
+        for (i in levels(data_train$euploid)){
+            tmp = data_train[data_train$euploid == i,c("euploid", "Ind")]
+            tmp$fold <- cut2(1:dim(tmp)[1], g = cv_fold)
+            levels(tmp$fold) = 1: cv_fold
+            cv_Ind[[i]] = tmp
+        }
+        
+        cv_Ind %<>% rbindlist() %>% setorder(Ind)
+        data_train %>% setorder(Ind)
+        
+        data_train$Ind <- NULL
 
+        for (foldid in 1:5){
+
+            cv_Ind_train = cv_Ind$fold != foldid
+            data_cvtrain = data_train[cv_Ind_train,]
+            cv_Ind_test = cv_Ind$fold == foldid
+            data_cvtest = data[cv_Ind_test,]
+            smote_train <- SMOTE(euploid ~ ., data  = data_train)
+            #print(table(smote_train$euploid))
+
+            Y_ <- smote_train$euploid
+            X_ <- smote_train
+            form <- euploid ~ .#*Female.Age
+            X <- model.matrix(form, data = X_)
+            Y <- Y_
+
+            model <- glmnet(X,Y,family="binomial",alpha = 0,lambda = lambdas_to_try[lambda],standardize = TRUE)
+            
+            y_real <- data_cvtest$euploid
+            x_pred_ <- data_cvtest
+            form <- euploid ~ .#*Female.Age
+            x_pred <- model.matrix(form, data = x_pred_)
+            y_pred <- predict(model, newx = x_pred, type = "response")
+            my_list <- get_performance_vals(y_real, y_pred)
+            score_fold[foldid] = my_list$fscore
+
+        }
+        score_params[lambda] =mean(as.numeric(score_fold))
+        model_params[[lambda]] = model
+
+    }
+    best_param = which.min(score_params)
+    print("F1 on crossval set: ")
+    print(score_params[best_param])
+    chosen_lambda = lambdas_to_try[best_param]
+    print("Chosen lambda: ")
+    print(chosen_lambda)
+    print("Index of chosen lambda: ")
+    print(best_param)
+    chosen_model = model_params[[best_param]]
+    
     y_real <- data_test$euploid
     x_pred_ <- data_test
-    x_pred_$euploid <- NULL
-    x_pred <- data.matrix(x_pred_)
-    y_pred <- predict(cv0, newx = x_pred, type = "response",  s="lambda.min")
+    form <- euploid ~ .#*Female.Age
+    x_pred <- model.matrix(form, data = x_pred_)
+    y_pred <- predict(chosen_model, newx = x_pred, type = "response")
 
-
-    areaundercurve <- prediction(y_pred, y_real) %>% performance(measure = "auc") %>% .@y.values %>% as.numeric()
     table(y_real, as.integer((y_pred > 0.5))==y_real) %>% prop.table()
     t <- table(y_real, as.integer((y_pred > 0.5))==y_real)
-
     print(t)
 
-    tn = t["0", "TRUE"] 
-    fn = t["1", "FALSE"]
-    tp = t["1", "TRUE"]
-    fp = t["0", "FALSE"]
-
-    precision = tp/(tp+fp) #1-FDR
-
-    if (tp==0){
-        recall = 0
-        F1 = 0
-    } else {
-        recall = tp/(tp+fn) #sensitivity, TPR
-        F1 = 2*precision*recall / (precision+recall)
-    }
-    specificity = tn/(tn+fp)
+    my_list <- get_performance_vals(y_real, y_pred)
+    areaundercurve = my_list$auc
+    F1 = my_list$fscore
+    recall = my_list$recall
+    specificity = my_list$specificity
+    mcc = my_list$mcc
+    accuracy = my_list$accuracy
+    precision = my_list$precision
 
     auc_list[j] <- areaundercurve
     f1_list[j] <- F1
     recall_list[j] <- recall
     specificity_list[j] <- specificity
+    mcc_list[j] <- mcc
+    accuracy_list[j] <- accuracy
+    precision_list[j] <- precision
 
     print("Predictions: ")
     print(cbind(y_real, y_pred))
@@ -148,16 +199,22 @@ for (j in 1:20) {
     print(specificity)
     print("Area under the curve: ")
     print(areaundercurve)
+    print("MCC: ")
+    print(mcc)
+    print("Accuracy: ")
+    print(accuracy)
+    print("Precision: ")
+    print(precision)
     print("Best cutoff and performance values: ")
     predi <- prediction(y_pred, y_real)
     roc.perf <- prediction(y_pred, y_real) %>% performance(measure = "tpr", x.measure = "fpr")
     print(opt.cut(roc.perf, predi))
 
     if (j==1) {
-        tmp_coeffs <- coef(cv0, s=cv0$lambda.min)
+        tmp_coeffs <- coef(chosen_model)
         df_coef <- data.frame(row.names = tmp_coeffs@Dimnames[[1]][tmp_coeffs@i + 1], coefficient = tmp_coeffs@x)
     } else {
-        tmp_coeffs <- coef(cv0, s=cv0$lambda.min)
+        tmp_coeffs <- coef(chosen_model)
         df_coef2 <- data.frame(row.names = tmp_coeffs@Dimnames[[1]][tmp_coeffs@i + 1], coefficient = tmp_coeffs@x)
         df_coef <- merge(df_coef, df_coef2, by="row.names")
         rownames(df_coef) = df_coef$Row.names
@@ -175,8 +232,20 @@ png("AUC.png")
 prediction(y_pred, y_real) %>% performance(measure = "tpr", x.measure = "fpr") %>% plot()
 dev.off()
 
+f1_list = as.numeric(f1_list)
+recall_list = as.numeric(recall_list)
+specificity_list= as.numeric(specificity_list)
+auc_list = as.numeric(auc_list)
+mcc_list = as.numeric(mcc_list)
+precision_list = as.numeric(precision_list)
+accuracy_list = as.numeric(accuracy_list)
+
 print(paste("Mean F1: ", mean(f1_list), "+/-", sd(f1_list)))
 print(paste("Mean recall: ", mean(recall_list), "+/-", sd(recall_list)))
 print(paste("Mean specificity: ", mean(specificity_list), "+/-", sd(specificity_list)))
 print(paste("Mean auc: ", mean(auc_list), "+/-", sd(auc_list)))
+print(paste("Mean mcc: ", mean(mcc_list), "+/-", sd(mcc_list)))
+print(paste("Mean precision: ", mean(precision_list), "+/-", sd(precision_list)))
+print(paste("Mean accuracy: ", mean(accuracy_list), "+/-", sd(accuracy_list)))
+
 
